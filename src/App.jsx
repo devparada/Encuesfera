4620a71b28d73f4cb6ff630236c643d112600545
Raúl <raul.parada040103@gmail.com>
2025-03-10 19:22:35 +0100

    Cambiado el logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import viteLogo from "/vite.svg";
+import logo from "/logo.png";
 import "./App.css";
 import Pregunta from "./components/Pregunta";
 import Opciones from "./components/Opciones";
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="inline">
       <a href="https://encuesfera.vercel.app" target="_blank">
-        <img src={viteLogo} alt="logo" width={"100px"} className="inline" />
+        <img src={logo} alt="logo" width={"100px"} className="inline" />
       </a>
       <div className="p-1.5">
         <h1 className="mt-2 mb-2">Bienvenido a Encuesfera</h1>
